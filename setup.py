6e09543fa87696d4fee41916c70c1143d2210604
GlassOnTin <63980135+GlassOnTin@users.noreply.github.com>
2024-11-15 21:22:37 +0000

    Bump version to 0.4.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gecko_controller",
-    version="0.4.4",
+    version="0.4.5",
     packages=find_packages(),
     install_requires=[
         "RPi.GPIO",
