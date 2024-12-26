4be31aa53915feeab00f0dc06e37ce3edb06394e
GlassOnTin <63980135+GlassOnTin@users.noreply.github.com>
2024-12-26 21:31:23 +0000

    Corrected MANIFEST.in filename case

--- setup.py
+++ setup.py
@@ -4,13 +4,20 @@ setup(
     name="gecko-controller",
     version="0.7.8",
     packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'gecko_controller.web': [
+            'templates/*',
+            'static/*',
+        ],
+    },
     install_requires=[
         "RPi.GPIO>=0.7.1",
         "smbus>=1.1.post2",
         "smbus2>=0.4.3",
         "Pillow>=10.2.0",
         "Flask>=2.2.2",
-	"pytest>=8.3.4",
+        "pytest>=8.3.4",
         "adafruit-circuitpython-busdevice>=5.2.6",
     ],
     entry_points={
@@ -19,5 +26,4 @@ setup(
             'gecko-web=gecko_controller.web.app:main',
         ],
     },
-    include_package_data=True,
 )
