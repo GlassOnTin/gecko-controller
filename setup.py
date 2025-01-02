58f70d7fa699c1cddf860d944c8b74a8a31b9947
GlassOnTin <63980135+GlassOnTin@users.noreply.github.com>
2025-01-02 01:24:47 +0000

    Bump version to 0.7.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gecko_controller",
-    version="0.7.8",
+    version="0.7.9",
     packages=find_packages(),
     package_data={
         'gecko_controller': [
